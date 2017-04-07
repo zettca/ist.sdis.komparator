34d8a0ac82360ed7376d3118bd19e4d6ee99321f
Bruno Henriques <zettca@gmail.com>
2017-04-07 22:38:04 +0100

    Fix base

--- mediator-ws-cli/src/test/java/org/komparator/mediator/ws/it/BaseIT.java
+++ mediator-ws-cli/src/test/java/org/komparator/mediator/ws/it/BaseIT.java
@@ -2,10 +2,12 @@ package org.komparator.mediator.ws.it;
 
 import java.io.IOException;
 import java.util.Properties;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.komparator.mediator.ws.cli.MediatorClient;
+import org.komparator.supplier.ws.ProductView;
+import org.komparator.supplier.ws.cli.SupplierClient;
+import org.komparator.supplier.ws.cli.SupplierClientException;
 
 public class BaseIT {
 
@@ -13,6 +15,8 @@ public class BaseIT {
 	protected static Properties testProps;
 
 	protected static MediatorClient mediatorClient;
+	protected static SupplierClient supplier1;
+	protected static SupplierClient supplier2;
 
 	@BeforeClass
 	public static void oneTimeSetup() throws Exception {
@@ -42,6 +46,40 @@ public class BaseIT {
 
 	@AfterClass
 	public static void cleanup() {
+	}
+
+	@BeforeClass
+	public static void populate() throws SupplierClientException {
+		supplier1 = new SupplierClient("http://t50:WkyodoJT@uddi.sd.rnl.tecnico.ulisboa.pt:9090/","T50_Komparator1");
+		supplier2 = new SupplierClient("http://t50:WkyodoJT@uddi.sd.rnl.tecnico.ulisboa.pt:9090/","T50_Komparator2");
+
+		ProductView product1 = new ProductView();
+		ProductView product2 = new ProductView();
+		ProductView product3 = new ProductView();
+
+		product1.setId("Batata");
+		product1.setDesc("Batatas sabem bem");
+		product1.setPrice(1);
+		product1.setQuantity(10);
+
+		product2.setId("Maca");
+		product2.setDesc("Macas sabem bem");
+		product2.setPrice(2);
+		product2.setQuantity(5);
+
+		product3.setId("Banana");
+		product3.setDesc("Bananas sabem bem");
+		product3.setPrice(3);
+		product3.setQuantity(30);
+
+		try {
+			supplier1.createProduct(product1);
+			supplier1.createProduct(product2);
+			supplier2.createProduct(product3);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
