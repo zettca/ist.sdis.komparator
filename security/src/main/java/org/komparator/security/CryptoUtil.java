8b887f813630c0d7af6f180e00d2944856c366fb
BrunoCarola <bruno.carola@tecnico.ulisboa.pt>
2017-04-30 20:10:51 +0100

    security helper methods done

--- security/src/main/java/org/komparator/security/CryptoUtil.java
+++ security/src/main/java/org/komparator/security/CryptoUtil.java
@@ -2,34 +2,94 @@ package org.komparator.security;
 
 import java.io.*;
 import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+
 import javax.crypto.*;
 import java.util.*;
+import static javax.xml.bind.DatatypeConverter.parseBase64Binary;
+import static javax.xml.bind.DatatypeConverter.printBase64Binary;
 
 
 
 public class CryptoUtil {
 
     // TODO add security helper methods
-	public byte[] asymCipher(byte[] plainBytes, PublicKey publicKey) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException{
+	public String asymCipher(byte[] plainBytes, PublicKey publicKey) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException{
 		
-		Cipher cipher = Cipher.getInstance("RSA");
+		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 		cipher.init(Cipher.ENCRYPT_MODE, publicKey);
 		byte[] cipherBytes = cipher.doFinal(plainBytes);
 		
-		//PARA IMPRIMIR ISTO USAR printBase64Binary EM CASO DE TESTE
+		String resultado = printBase64Binary(cipherBytes);
 		
-		return cipherBytes;
+		return resultado;
 	}
 	
-	public byte[] asymDecipher(byte[] cipherBytes, PrivateKey privateKey) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException{
+	public byte[] asymDecipher(String cipherText, PrivateKey privateKey) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException{
 		
-		Cipher cipher = Cipher.getInstance("RSA");
+		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
 		cipher.init(Cipher.DECRYPT_MODE, privateKey); 
-		byte[] plainBytes = cipher.doFinal(cipherBytes); 
 		
-		//PARA IMPRIMIR ISTO USAR parseBase64Binary EM CASO DE TESTE
+		byte[] plainBytes = parseBase64Binary(cipherText);
 		
-		return plainBytes;
-
+		byte[] resultado = cipher.doFinal(plainBytes); 
+		
+		return resultado;
 	}
+	
+	public boolean verifyDigitalSignature(byte[] cipherDigest, byte[] bytes, PublicKey publicKey)
+			throws Exception {
+
+		// verify the signature with the public key
+		Signature sig = Signature.getInstance("SHA256withRSA");
+		sig.initVerify(publicKey);
+		sig.update(bytes);
+		try {
+			return sig.verify(cipherDigest);
+		} catch (SignatureException se) {
+			System.err.println("Caught exception while verifying signature " + se);
+			return false;
+		}
+	}
+	
+	public byte[] makeDigitalSignature(byte[] bytes, PrivateKey privateKey) throws Exception {
+
+		// get a signature object using the SHA-1 and RSA combo
+		// and sign the plain-text with the private key
+		Signature sig = Signature.getInstance("SHA1WithRSA");
+		sig.initSign(privateKey);
+		sig.update(bytes);
+		byte[] signature = sig.sign();
+
+		return signature;
+	}
+	
+	public KeyStore readKeystoreFile(String keyStoreFilePath, char[] keyStorePassword) throws Exception {
+		FileInputStream fis;
+		try {
+			fis = new FileInputStream(keyStoreFilePath);
+		} catch (FileNotFoundException e) {
+			System.err.println("Keystore file <" + keyStoreFilePath + "> not found.");
+			return null;
+		}
+		KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+		keystore.load(fis, keyStorePassword);
+		return keystore;
+	}
+	
+	public PrivateKey getPrivateKeyFromKeystore(String keyStoreFilePath, char[] keyStorePassword,
+			String keyAlias, char[] keyPassword) throws Exception {
+
+		KeyStore keystore = readKeystoreFile(keyStoreFilePath, keyStorePassword);
+		PrivateKey key = (PrivateKey) keystore.getKey(keyAlias, keyPassword);
+
+		return key;
+	}
+	
+	public PublicKey getPublicKeyFromCertificate(Certificate certificate) {
+		return certificate.getPublicKey();
+	}
+	
+	
 }
