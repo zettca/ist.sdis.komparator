78ae8cb024ac93aa3791a170fec3268dd0e35ece
BrunoCarola <bruno.carola@tecnico.ulisboa.pt>
2017-04-30 20:14:52 +0100

    added some operations on CA

--- ca-ws-cli/src/main/java/pt/ulisboa/tecnico/sdis/ws/cli/CAClient.java
+++ ca-ws-cli/src/main/java/pt/ulisboa/tecnico/sdis/ws/cli/CAClient.java
@@ -2,6 +2,11 @@ package pt.ulisboa.tecnico.sdis.ws.cli;
 
 import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
 import java.util.Map;
 
 import javax.xml.ws.BindingProvider;
@@ -104,5 +109,48 @@ public class CAClient implements CA {
 	public String getCertificate(String certificateName) {
 		return port.getCertificate(certificateName);
 	}
+	
+	
+	public byte[] getPublicKey(String entity){
+		
+		Certificate c = null;
+		
+		try {
+			if(entity.equals("ca")){
+				c = readCertificateFile("../ca-ws/src/main/resources/ca.cer");
+			}else{
+				c = readCertificateFile("../ca-ws/src/main/resources/" + entity + ".cer");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return c.getPublicKey().getEncoded();
+	}
+	
+	public static Certificate readCertificateFile(String certificateFilePath) throws Exception {
+		FileInputStream fis;
+
+		try {
+			fis = new FileInputStream(certificateFilePath);
+		} catch (FileNotFoundException e) {
+			System.err.println("Certificate file <" + certificateFilePath + "> not fount.");
+			return null;
+		}
+		BufferedInputStream bis = new BufferedInputStream(fis);
+
+		CertificateFactory cf = CertificateFactory.getInstance("X.509");
+
+		if (bis.available() > 0) {
+			Certificate cert = cf.generateCertificate(bis);
+			return cert;
+			// It is possible to print the content of the certificate file:
+			// System.out.println(cert.toString());
+		}
+		bis.close();
+		fis.close();
+		return null;
+	}
+
 
 }
