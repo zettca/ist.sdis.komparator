68c80cc0ae5774061e7c7e2e60faa86a605c5d07
Bruno Henriques <zettca@gmail.com>
2017-05-04 03:36:33 +0100

    Add CC Cypher

--- ws-handlers/src/main/java/example/ws/handler/CypherCCHandler.java
+++ ws-handlers/src/main/java/example/ws/handler/CypherCCHandler.java
@@ -1,14 +1,25 @@
 package example.ws.handler;
 
+import org.komparator.security.CryptoUtil;
+import pt.ulisboa.tecnico.sdis.ws.cli.CAClient;
+import pt.ulisboa.tecnico.sdis.ws.cli.CAClientException;
+
 import javax.xml.namespace.QName;
 import javax.xml.soap.*;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Iterator;
 import java.util.Set;
 
+import static javax.xml.bind.DatatypeConverter.printBase64Binary;
+
 public class CypherCCHandler implements SOAPHandler<SOAPMessageContext> {
+    private CAClient ca = setUp("http://sec.sd.rnl.tecnico.ulisboa.pt:8081/ca");
 
     @Override
     public Set<QName> getHeaders() {
@@ -25,19 +36,35 @@ public class CypherCCHandler implements SOAPHandler<SOAPMessageContext> {
         try {
             SOAPEnvelope env = smc.getMessage().getSOAPPart().getEnvelope();
             SOAPBody sb = env.getBody();
-            Name name = env.createName("buyCart", "ns2", "http://ws.mediator.komparator.org/"); // not sure
+            Name name = env.createName("buyCart"/*, "ns2", "http://ws.mediator.komparator.org/"*/); // not sure
             Iterator it = sb.getChildElements(name);
             if (!it.hasNext()) {
-                System.out.println("No BuyCart? Unexpected...");
+                System.out.println("No BuyCart? Should be response...");
+                return true;
             }
 
             SOAPElement el = (SOAPElement) it.next();
+            name = env.createName("creditCardNr");
+            it = el.getChildElements(name);
+            if (!it.hasNext()) {
+                System.out.println("No creditCardNr entry? Totally unexpected...");
+                return false;
+            }
+
+            /* Mediator-cli sending to Mediator */
+
+            el = (SOAPElement) it.next();
             String textCC = el.getValue();
 
-            String cypheredCC = "";
-            // TODO: Do Cypher CC
+            byte[] caBytes = ca.getPublicKey("T50_Mediator");
+            PublicKey key = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(caBytes));
+
+            String cypheredCC = CryptoUtil.asymCipher(textCC.getBytes(), key);
+            el.setValue(cypheredCC);
 
         } catch (SOAPException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -48,20 +75,38 @@ public class CypherCCHandler implements SOAPHandler<SOAPMessageContext> {
         try {
             SOAPEnvelope env = smc.getMessage().getSOAPPart().getEnvelope();
             SOAPBody sb = env.getBody();
-            Name name = env.createName("buyCartResponse", "ns2", "http://ws.mediator.komparator.org/"); // not sure
+            Name name = env.createName("buyCart"/*, "ns2", "http://ws.mediator.komparator.org/"*/); // not sure
             Iterator it = sb.getChildElements(name);
             if (!it.hasNext()) {
-                System.out.println("No BuyCart? Unexpected...");
+                System.out.println("No BuyCart? Should be response...");
+                return true;
             }
 
             SOAPElement el = (SOAPElement) it.next();
+            name = env.createName("creditCardNr");
+            it = el.getChildElements(name);
+            if (!it.hasNext()) {
+                System.out.println("No creditCardNr entry? Totally unexpected...");
+            }
+
+            el = (SOAPElement) it.next();
             String cypheredCC = el.getValue();
 
+            /* Mediator-cli sending to Mediator */
+
+
             // TODO: Do DeCypher CC
-            String textCC = "";
+            String keyPath = "";
+            char[] pass = "WkyodoJT".toCharArray();
+            PrivateKey key = CryptoUtil.getPrivateKeyFromKeystore(keyPath, pass, "T50_Mediator", pass);
+            byte[] bytesCC = CryptoUtil.asymDecipher(el.getValue(), key);
+            String textCC = printBase64Binary(bytesCC);
+            el.setValue(textCC);
 
 
         } catch (SOAPException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -76,6 +121,18 @@ public class CypherCCHandler implements SOAPHandler<SOAPMessageContext> {
     @Override
     public void close(MessageContext messageContext) {
         // nothing to clean up
+    }
+
+    public CAClient setUp(String url) {
+        if (ca == null) {
+            try {
+                ca = new CAClient(url);
+            } catch (CAClientException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return ca;
     }
 
 }
