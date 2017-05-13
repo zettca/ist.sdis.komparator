eeeaaf1b6d91c463f0b0eec17fd60cfd825f2155
Bruno Henriques <zettca@gmail.com>
2017-05-13 01:49:48 +0100

    Add LifeProof empty class

---
+++ mediator-ws/src/main/java/org/komparator/mediator/ws/LifeProof.java
@@ -0,0 +1,18 @@
+package org.komparator.mediator.ws;
+
+import java.util.TimerTask;
+
+public class LifeProof extends TimerTask {
+
+    public LifeProof() {
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Checking if Mediator is alive...");
+        // this method will be called every for every loop thingy.
+
+        // TODO: implement this
+        
+    }
+}
